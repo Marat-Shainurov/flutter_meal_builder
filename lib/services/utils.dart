3f0b33583a7e541062ac39d127f2406c4aaf4f6f
Marat-Shainurov <m_shainurov@mail.ru>
2024-08-21 16:57:41 +0400

    add showLoaderDialog method to utils

--- lib/services/utils.dart
+++ lib/services/utils.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class Utils {
   String formatTimeAgo(Duration duration) {
     if (duration.inDays > 7) {
@@ -13,5 +15,28 @@ class Utils {
     } else {
       return "just now";
     }
+  }
+
+  void showLoaderDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible:
+          false, // Prevent closing the dialog by tapping outside
+      builder: (BuildContext context) {
+        return Dialog(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                CircularProgressIndicator(),
+                SizedBox(width: 20),
+                Text("Loading..."),
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 }
