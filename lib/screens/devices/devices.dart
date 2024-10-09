4ce498d018eeee6e09f6139e80d9a74049e2d522
Marat-Shainurov <m_shainurov@mail.ru>
2024-10-09 11:40:15 +0400

    install flutter_serial_communication, upd the device widget

--- lib/screens/devices/devices.dart
+++ lib/screens/devices/devices.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_libserialport/flutter_libserialport.dart';
 import 'dart:io';
+import 'package:flutter_serial_communication/flutter_serial_communication.dart';
+import 'package:flutter_serial_communication/models/device_info.dart';
 import 'dart:typed_data';
-import 'package:path_provider/path_provider.dart';
-import 'dart:convert';
 
 class Devices extends StatefulWidget {
   const Devices({super.key});
@@ -13,40 +12,130 @@ class Devices extends StatefulWidget {
 }
 
 class _DevicesState extends State<Devices> {
-  List<String> availableDevices = [];
+  List<DeviceInfo> availableDevices = [];
   String weight = '';
+  String rawDataString = '';
+  bool isPopupVisible = false;
   File? logFile;
+  final _flutterSerialCommunicationPlugin = FlutterSerialCommunication();
+  bool isConnected = false;
 
   @override
   void initState() {
     super.initState();
-    _initializeLogFile();
     _scanDevices();
-    // _requestPermissions();
   }
 
-  Future<void> _initializeLogFile() async {
-    // Get the directory path where the log file will be stored
-    Directory appDocDirectory = await getApplicationDocumentsDirectory();
-    String logFilePath = '${appDocDirectory.path}/scale_logs.txt';
-    logFile = File(logFilePath);
-    await logFile?.writeAsString('Log initialized on ${DateTime.now()}\n');
-    print('Log file initialized at: $logFilePath');
-  }
+  void _scanDevices() async {
+    try {
+      List<DeviceInfo> devices =
+          await _flutterSerialCommunicationPlugin.getAvailableDevices();
+      setState(() {
+        availableDevices = devices;
+      });
 
-  Future<void> _writeToLogFile(String message) async {
-    if (logFile != null) {
-      await logFile!.writeAsString('$message\n', mode: FileMode.append);
+      print(
+          'Available devices: ${devices.map((d) => d.productName).join(", ")}');
+    } catch (e) {
+      print('Error scanning devices: $e');
     }
   }
 
-  void _scanDevices() {
-    setState(() {
-      // Get list of all available USB serial devices
-      availableDevices = SerialPort.availablePorts;
-      print('availableDevices ******** $availableDevices');
-      _writeToLogFile('Available devices: $availableDevices');
+  void _connectToDevice(DeviceInfo device) async {
+    try {
+      int baudRate = 9600;
+
+      bool isConnectionSuccess =
+          await _flutterSerialCommunicationPlugin.connect(device, baudRate);
+
+      if (isConnectionSuccess) {
+        print('Connected to device: ${device.productName}');
+
+        await _flutterSerialCommunicationPlugin.setParameters(
+            baudRate, 8, 1, 0);
+
+        setState(() {
+          isConnected = true;
+        });
+
+        _startListeningForWeight();
+      } else {
+        print('Failed to connect to device: ${device.productName}');
+      }
+    } catch (e) {
+      print('Error connecting to device: $e');
+    }
+  }
+
+  void _startListeningForWeight() {
+    _flutterSerialCommunicationPlugin
+        .getSerialMessageListener()
+        .receiveBroadcastStream()
+        .listen((event) {
+      try {
+        // Raw data from the scales
+        Uint8List rawData = event;
+
+        // Decoded data (interpreted as string/weight)
+        String decodedData = String.fromCharCodes(rawData);
+
+        // Ensure decodedData is valid and contains weight data
+        if (decodedData.trim().isNotEmpty) {
+          setState(() {
+            weight = decodedData;
+            rawDataString = rawData
+                .map((e) => e.toRadixString(16).padLeft(2, '0'))
+                .join(' ');
+          });
+
+          // Show or update the weight dialog with throttling
+          if (!isPopupVisible) {
+            _showWeightDialog(decodedData, rawData);
+          } else {
+            // Update the popup contents if already visible
+            Navigator.of(context).pop(); // Close previous popup
+            _showWeightDialog(decodedData, rawData); // Open new popup
+          }
+        }
+      } catch (e) {
+        print('Error decoding data: $e');
+      }
     });
+  }
+
+  void _showWeightDialog(String decodedData, Uint8List rawData) {
+    // Mark the popup as visible
+    isPopupVisible = true;
+
+    showDialog(
+      context: context,
+      barrierDismissible: false, // Prevent accidental dismissals
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Weight"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text("Decoded Weight: $decodedData"),
+              SizedBox(height: 16.0),
+              Text(
+                  "Raw Data: ${rawData.map((e) => e.toRadixString(16).padLeft(2, '0')).join(' ')}"),
+            ],
+          ),
+          actions: [
+            TextButton(
+              child: Text("Close"),
+              onPressed: () {
+                isPopupVisible = false; // Mark the popup as closed
+                Navigator.of(context).pop(); // Close the dialog
+              },
+            ),
+          ],
+        );
+      },
+    );
+    print(
+        'Displayed weight: $decodedData, Raw Data: ${rawData.map((e) => e.toRadixString(16).padLeft(2, '0')).join(' ')}');
   }
 
   @override
@@ -59,155 +148,13 @@ class _DevicesState extends State<Devices> {
         itemCount: availableDevices.length,
         itemBuilder: (context, index) {
           return ListTile(
-            title: Text(availableDevices[index]),
+            title: Text(availableDevices[index].productName),
             onTap: () {
-              // On tap, you can handle connecting to the selected device
               _connectToDevice(availableDevices[index]);
             },
           );
         },
       ),
     );
-  }
-
-  void _connectToDevice(String port) async {
-    try {
-      final SerialPort serialPort = SerialPort(port);
-      if (serialPort.openReadWrite()) {
-        _writeToLogFile('Connected to $port');
-
-        // var config = SerialPortConfig();
-        // config.baudRate = 9600;
-        // config.bits = 8;
-        // config.parity = SerialPortParity.none;
-        // config.stopBits = 1;
-        // config.setFlowControl(SerialPortFlowControl.none);
-
-        // serialPort.config = config;
-        // _writeToLogFile(
-        //     'Serial Port Config: BaudRate: ${config.baudRate}, Bits: ${config.bits}, Parity: ${config.parity}, StopBits: ${config.stopBits}');
-        // config.dispose();
-
-        var config = SerialPortConfig()
-          ..baudRate = 9600
-          ..bits = 8
-          ..parity = SerialPortParity.none
-          ..stopBits = 1
-          ..xonXoff = 0
-          ..rts = 1
-          ..cts = 0
-          ..dsr = 0
-          ..dtr = 1;
-        serialPort.config = config;
-        config.dispose();
-
-        final reader = SerialPortReader(serialPort, timeout: 3000);
-        reader.stream.listen((data) {
-          try {
-            final decodedData = String.fromCharCodes(data);
-            _writeToLogFile('Decoded data: $decodedData');
-            _writeToLogFile('Raw data: $data');
-
-            // if (!_isASCII(data)) {
-            //   _writeToLogFile('Warning: Non-ASCII data detected.');
-            //   // _logPossibleDataTypes(
-            //   //     data); // Log possible non-ASCII data formats
-            // }
-
-            _showWeightDialog(decodedData);
-          } catch (e) {
-            // Log any errors during decoding
-            _writeToLogFile('Error decoding data: $e');
-          }
-        });
-      } else {
-        _writeToLogFile('Failed to open port: $port');
-      }
-    } catch (e) {
-      _writeToLogFile('Error: $e');
-    }
-  }
-
-  void _logDataInfo(Uint8List data) {
-    // Log the raw byte data before any decoding
-    _writeToLogFile('Raw byte data: $data');
-    for (var byte in data) {
-      _writeToLogFile(
-          'Byte: $byte (${byte.toRadixString(16).toUpperCase()} in hex)');
-    }
-
-    // Log the length of the data received
-    _writeToLogFile('Data length: ${data.length}');
-  }
-
-// Check if the data is ASCII
-  bool _isASCII(Uint8List data) {
-    return data
-        .every((byte) => byte >= 32 && byte <= 126); // Printable ASCII range
-  }
-
-// Log possible data types if non-ASCII data is detected
-  void _logPossibleDataTypes(Uint8List data) {
-    // Log as hexadecimal
-    String hexString =
-        data.map((byte) => byte.toRadixString(16).padLeft(2, '0')).join(' ');
-    _writeToLogFile('Data in hex: $hexString');
-
-    // Log as binary
-    String binaryString =
-        data.map((byte) => byte.toRadixString(2).padLeft(8, '0')).join(' ');
-    _writeToLogFile('Data in binary: $binaryString');
-
-    // Try decoding as UTF-8 (in case it's a different text encoding)
-    try {
-      String utf8Data = utf8.decode(data);
-      _writeToLogFile('Decoded data (UTF-8): $utf8Data');
-    } catch (e) {
-      _writeToLogFile('Failed to decode data as UTF-8: $e');
-    }
-
-    // Log a possible integer interpretation (e.g., weight could be encoded as integers)
-    if (data.length >= 2) {
-      int integerValue = _parseInteger(data);
-      _writeToLogFile('Interpreted data as integer: $integerValue');
-    }
-  }
-
-// Helper function to parse integer values from the byte array
-  int _parseInteger(Uint8List data) {
-    // Assume big-endian and 16-bit or 32-bit integer based on data length
-    int result = 0;
-    for (var byte in data) {
-      result = (result << 8) | byte;
-    }
-    return result;
-  }
-
-  void _showWeightDialog(String decodedData) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Weight Data'),
-          content: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Weight: $decodedData',
-                  style: TextStyle(fontWeight: FontWeight.bold)),
-              SizedBox(height: 10),
-            ],
-          ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('OK'),
-            ),
-          ],
-        );
-      },
-    );
-    _writeToLogFile('Displayed weight: $decodedData');
   }
 }
